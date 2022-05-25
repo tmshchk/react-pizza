e02385a22ee1f40086aedccd76f835d16f9f9b1b
Alexandr Timoshchuk <ingword@gmail.com>
2022-05-25 14:30:07 +0300

    get items from backend, fix styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './scss/app.scss';
-
+import React from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://628df452a339dfef87a46bad.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,9 +28,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj}
-              />
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
         </div>
