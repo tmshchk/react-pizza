ea612cee28134b154e4682b834bc3f2ecced5616
Alexander <ingword@gmail.com>
2022-06-05 14:37:10 +0300

    add debounce

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 import styles from './Search.module.scss';
 import icon from '../../assets/img/search_icon.svg'
@@ -6,20 +7,39 @@ import close from '../../assets/img/close_icon.svg'
 import { SearchContext } from '../../App';
 
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('');
+  const { setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    setValue('');
+    inputRef.current.focus();
+  }
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 500),
+    [],
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
       <img className={styles.icon} src={icon} alt="Поиск" />
       <input
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы..."
-        value={searchValue}
+        value={value}
       />
-      {searchValue && (
-        <img onClick={() => setSearchValue('')} className={styles.clear} src={close} alt="Очистить" />
-      )}
+      {value && <img onClick={onClickClear} className={styles.clear} src={close} alt="Очистить" />}
     </div>
   );
 };
